df6bdffc1a733fce74d50d8c5fe9f80c750ecc8f
Sasiafril <sasiafrila8513@gmail.com>
2022-01-19 10:54:42 +0700

    Modul2

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "nama" => "Sasi Afrila Putri P.",
         "email" => "sasiafrila8513@gmail.com",
         "gambar" => "28_Sasi Afrila.png"
@@ -26,5 +29,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
